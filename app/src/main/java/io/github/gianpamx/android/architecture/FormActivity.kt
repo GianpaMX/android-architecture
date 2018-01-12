6badf418309a4a8fcab369e7d57f1df037328ea4
Juan Villa <juan64@gmail.com>
2018-01-12 01:38:17 -0600

    first commit

---
+++ app/src/main/java/io/github/gianpamx/android/architecture/FormActivity.kt
@@ -0,0 +1,13 @@
+package io.github.gianpamx.android.architecture
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import io.github.gianpamx.androidarchitecure.R
+
+class FormActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.form_activity)
+    }
+}
