c62732c567463ec604a1535a914530bcd7139831
Juan Pablo "Gianpa" Villa <juan64@gmail.com>
2018-03-14 18:31:16 -0600

    Room Database exportSchema = false

--- app/src/main/java/io/github/gianpamx/android/architecture/data/room/AppDatabase.kt
+++ app/src/main/java/io/github/gianpamx/android/architecture/data/room/AppDatabase.kt
@@ -3,7 +3,7 @@ package io.github.gianpamx.android.architecture.data.room
 import android.arch.persistence.room.Database
 import android.arch.persistence.room.RoomDatabase
 
-@Database(entities = arrayOf(FormRoom::class), version = 1)
+@Database(entities = arrayOf(FormRoom::class), version = 1, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun formDao(): FormDao
 }
